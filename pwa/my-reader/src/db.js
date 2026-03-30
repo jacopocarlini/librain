2cdcca1c34fa0035d6f6693e66039fcad11e0410
Jacopo Carlini <jacopo.carlini@gmail.com>
2026-03-30 13:28:23 +0200

    fix

--- pwa/my-reader/src/db.js
+++
@@ -1,7 +0,0 @@
-import Dexie from 'dexie';
-
-export const db = new Dexie('eReaderDB');
-db.version(1).stores({
-    books: '++id, title, progress, currentCfi'
-    // 'file' (ArrayBuffer) e 'cover' (Blob/URL) saranno salvati ma non serve indicizzarli
-});
